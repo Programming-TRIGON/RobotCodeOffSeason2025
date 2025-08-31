bd83f49cdc723a3e8b5fb9bd05f11cc79fd99cc1
Strflightmight09 <148347057+Strflightmight09@users.noreply.github.com>
2025-08-31 22:23:14 +0300

    Better logic

--- src/main/java/frc/trigon/robot/commands/commandfactories/ClimbCommands.java
+++ src/main/java/frc/trigon/robot/commands/commandfactories/ClimbCommands.java
@@ -1,20 +1,33 @@
 package frc.trigon.robot.commands.commandfactories;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.trigon.robot.RobotContainer;
 import frc.trigon.robot.constants.OperatorConstants;
 import frc.trigon.robot.subsystems.climber.ClimberCommands;
 import frc.trigon.robot.subsystems.climber.ClimberConstants;
+import frc.trigon.robot.subsystems.swerve.SwerveCommands;
 
 public class ClimbCommands {
+    public static boolean IS_CLIMBING = false;
+
     public static Command getClimbCommand() {
         return new SequentialCommandGroup(
+                new InstantCommand(() -> IS_CLIMBING = true),
                 ClimberCommands.getSetTargetStateCommand(ClimberConstants.ClimberState.PREPARE_FOR_CLIMB)
-                        .until(RobotContainer.CLIMBER::hasCage),
+                        .until(() -> RobotContainer.CLIMBER.hasCage() || OperatorConstants.CONTINUE_TRIGGER.getAsBoolean()),
                 ClimberCommands.getSetTargetStateCommand(ClimberConstants.ClimberState.CLIMB)
                         .until(RobotContainer.CLIMBER::atTargetState),
-                ClimberCommands.getSetTargetSpeedCommand(OperatorConstants.DRIVER_CONTROLLER.getRightY())
-        );
+                new ParallelCommandGroup(
+                        ClimberCommands.getSetTargetSpeedCommand(OperatorConstants.DRIVER_CONTROLLER.getRightY()),
+                        SwerveCommands.getClosedLoopSelfRelativeDriveCommand(
+                                () -> 0,
+                                () -> 0,
+                                () -> 0
+                        )
+                )
+        ).finallyDo(() -> IS_CLIMBING = false);
     }
 }
