434bb33e6d00cb264a558440698b4cabd4cbff19
ShmayaR <shmaya.rosenblatt@gmail.com>
2025-09-01 19:04:32 +0300

    Update ArmCommands.java

--- src/main/java/frc/trigon/robot/subsystems/arm/ArmCommands.java
+++ src/main/java/frc/trigon/robot/subsystems/arm/ArmCommands.java
@@ -1,16 +1,14 @@
 package frc.trigon.robot.subsystems.arm;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import frc.trigon.robot.RobotContainer;
 
 public class ArmCommands {
-    public static Command setTargetPositionCommand(ArmConstants.ArmState targetState) {
-        return new FunctionalCommand(
+    public static Command getSetTargetStateCommand(ArmConstants.ArmState targetState) {
+        return new StartEndCommand(
                 () -> RobotContainer.ARM.setTargetState(targetState),
-                () -> RobotContainer.ARM.setTargetState(targetState),
-                (interrupted) -> RobotContainer.ARM.setTargetState(ArmConstants.ArmState.REST),
-                () -> false,
+                RobotContainer.ARM::stop,
                 RobotContainer.ARM
         );
     }
