a3dc280dd1ac0b6fa268d697d8f5db386d906868
ShmayaR <shmaya.rosenblatt@gmail.com>
2025-09-02 16:57:09 +0300

    updated robotPeriodic

--- src/main/java/frc/trigon/robot/Robot.java
+++ src/main/java/frc/trigon/robot/Robot.java
@@ -5,6 +5,7 @@
 
 package frc.trigon.robot;
 
+import edu.wpi.first.wpilibj.Threads;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.trigon.robot.constants.RobotConstants;
@@ -15,6 +16,7 @@ import org.littletonrobotics.junction.networktables.NT4Publisher;
 import org.littletonrobotics.junction.wpilog.WPILOGReader;
 import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 import trigon.hardware.RobotHardwareStats;
+import trigon.hardware.phoenix6.Phoenix6Inputs;
 
 public class Robot extends LoggedRobot {
     public static final boolean IS_REAL = Robot.isReal();
@@ -30,8 +32,10 @@ public class Robot extends LoggedRobot {
 
     @Override
     public void robotPeriodic() {
+        Threads.setCurrentThreadPriority(true, 99);
+        Phoenirx6Inputs.refreshAllInputs();
         commandScheduler.run();
-    }
+        Threads.setCurrentThreadPriority(false, 10);    }
 
     @Override
     public void autonomousInit() {
