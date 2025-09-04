30ccb76859b273b301e13441eac4be7dab913dc1
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2025-09-05 02:04:06 +0300

    better stuff

--- src/main/java/frc/trigon/robot/constants/LEDConstants.java
+++ src/main/java/frc/trigon/robot/constants/LEDConstants.java
@@ -1,7 +1,14 @@
 package frc.trigon.robot.constants;
 
+import edu.wpi.first.wpilibj.util.Color;
+import lib.hardware.misc.leds.LEDStripAnimationSettings;
+
 public class LEDConstants {
-    //TODO: Implement LEDConstants
+    public static LEDStripAnimationSettings.ColorFlowSettings COLLECTION_CONFIRMATION_ANIMATION_SETTINGS = new LEDStripAnimationSettings.ColorFlowSettings(
+            Color.kGreen,
+            0.5,
+            false
+    );
 
     /**
      * Initializes LEDConstants. Needed to be called for the LED strips to be initialized before being used.
