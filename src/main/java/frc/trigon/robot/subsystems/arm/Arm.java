5e9fa2a56b793a0da2fb0b78173c99ce0728a92d
ShmayaR <shmaya.rosenblatt@gmail.com>
2025-09-05 05:31:08 +0300

    Arm (#5)
    * added arm susbsystem
    
    * added arm subsystem
    
    * Update ArmConstants.java
    
    * Update keyboard_to_nt.py
    
    * Update RobotContainer.java
    
    * Update ArmConstants.java
    
    * removed follower in subsystem class
    
    * added reverse scoring for enum and voltage for endeffector
    
    * Update Arm.java
    
    * Update ArmCommands.java
    
    * added visualiation oofset
    
    * added debugging command
    
    * Update ArmConstants.java
    
    * added logic for the arm
    
    * Update Arm.java
    
    * changed names and started on end effector
    
    * Update gradlew
    
    * minor fix
    
    * added endeffector
    
    * changed logic of arm to avoid hitting intake. added visualization offset to make arm face down in sim.
    
    * changed gains to 0
    
    * added feedforward and pid gains
    
    * Update Arm.java
    
    * changed debugging command
    
    * added distance sensor
    
    * added more states to enum
    
    * added gear ratio calculation command
    
    * added distance sensor to update periodically
    
    * added current limits
    
    * Update RobotContainer.java
    
    * created function for setting the targetState with reverse states rather than having the reverse states in the enum
    
    * updated from main
    
    * Update ArmConstants.java
    
    * updated endeffector mechanism when setting oltage
    
    * renamed stuff
    
    * renameing
    
    * changed name
    
    * ZEXRDCFTVGYBHJN

---
+++ src/main/java/frc/trigon/robot/subsystems/arm/Arm.java
@@ -0,0 +1,140 @@
+package frc.trigon.robot.subsystems.arm;
+
+import com.ctre.phoenix6.controls.DynamicMotionMagicVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
+import edu.wpi.first.math.geometry.*;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.wpilibj.sysid.SysIdRoutineLog;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.trigon.robot.subsystems.MotorSubsystem;
+import lib.hardware.phoenix6.cancoder.CANcoderEncoder;
+import lib.hardware.phoenix6.cancoder.CANcoderSignal;
+import lib.hardware.phoenix6.talonfx.TalonFXMotor;
+import lib.hardware.phoenix6.talonfx.TalonFXSignal;
+import org.littletonrobotics.junction.Logger;
+
+public class Arm extends MotorSubsystem {
+    private final TalonFXMotor
+            armMasterMotor = ArmConstants.ARM_MASTER_MOTOR,
+            endEffectorMotor = ArmConstants.END_EFFECTOR_MOTOR;
+    private final CANcoderEncoder angleEncoder = ArmConstants.ANGLE_ENCODER;
+    private final VoltageOut voltageRequest = new VoltageOut(0).withEnableFOC(ArmConstants.FOC_ENABLED);
+    private final DynamicMotionMagicVoltage positionRequest = new DynamicMotionMagicVoltage(
+            0,
+            ArmConstants.ARM_DEFAULT_MAXIMUM_VELOCITY,
+            ArmConstants.ARM_DEFAULT_MAXIMUM_ACCELERATION,
+            ArmConstants.ARM_DEFAULT_MAXIMUM_JERK
+    ).withEnableFOC(ArmConstants.FOC_ENABLED);
+    private ArmConstants.ArmState targetState = ArmConstants.ArmState.REST;
+
+    public Arm() {
+        setName("Arm");
+    }
+
+    @Override
+    public SysIdRoutine.Config getSysIDConfig() {
+        return ArmConstants.ARM_SYSID_CONFIG;
+    }
+
+    @Override
+    public void setBrake(boolean brake) {
+        armMasterMotor.setBrake(brake);
+    }
+
+    @Override
+    public void stop() {
+        armMasterMotor.stopMotor();
+        endEffectorMotor.stopMotor();
+    }
+
+    @Override
+    public void updateLog(SysIdRoutineLog log) {
+        log.motor("Arm")
+                .angularPosition(Units.Rotations.of(getAngle().getRotations()))
+                .angularVelocity(Units.RotationsPerSecond.of(armMasterMotor.getSignal(TalonFXSignal.VELOCITY)))
+                .voltage(Units.Volts.of(armMasterMotor.getSignal(TalonFXSignal.MOTOR_VOLTAGE)));
+    }
+
+    @Override
+    public void updateMechanism() {
+        ArmConstants.ARM_MECHANISM.update(
+                Rotation2d.fromRotations(getAngle().getRotations() + ArmConstants.POSITION_OFFSET_FROM_GRAVITY_OFFSET),
+                Rotation2d.fromRotations(armMasterMotor.getSignal(TalonFXSignal.CLOSED_LOOP_REFERENCE) + ArmConstants.POSITION_OFFSET_FROM_GRAVITY_OFFSET)
+        );
+        ArmConstants.END_EFFECTOR_MECHANISM.update(endEffectorMotor.getSignal(TalonFXSignal.MOTOR_VOLTAGE));
+
+        Logger.recordOutput("Poses/Components/ArmPose", calculateVisualizationPose());
+    }
+
+    @Override
+    public void updatePeriodically() {
+        armMasterMotor.update();
+        endEffectorMotor.update();
+        angleEncoder.update();
+        ArmConstants.DISTANCE_SENSOR.updateSensor();
+        Logger.recordOutput("Arm/CurrentPositionDegrees", getAngle().getDegrees());
+    }
+
+    @Override
+    public void sysIDDrive(double targetVoltage) {
+        armMasterMotor.setControl(voltageRequest.withOutput(targetVoltage));
+    }
+
+    public boolean atState(ArmConstants.ArmState targetState) {
+        return this.targetState == targetState && atTargetAngle();
+    }
+
+    public boolean atTargetAngle() {
+        final double currentToTargetStateDifferenceDegrees = Math.abs(targetState.targetAngle.minus(getAngle()).getDegrees());
+        return currentToTargetStateDifferenceDegrees < ArmConstants.ANGLE_TOLERANCE.getDegrees();
+    }
+
+    public boolean hasGamePiece() {
+        return ArmConstants.COLLECTION_DETECTION_BOOLEAN_EVENT.getAsBoolean();
+    }
+
+    void setTargetState(ArmConstants.ArmState targetState, boolean isStateReversed) {
+        if (isStateReversed) {
+            setTargetState(
+                    Rotation2d.fromDegrees(360 - targetState.targetAngle.getDegrees())
+                    , targetState.targetEndEffectorVoltage
+            );
+            return;
+        }
+        setTargetState(targetState);
+    }
+
+    void setTargetState(ArmConstants.ArmState targetState) {
+        this.targetState = targetState;
+        setTargetState(
+                targetState.targetAngle,
+                targetState.targetEndEffectorVoltage
+        );
+    }
+
+    void setTargetState(Rotation2d targetAngle, double targetVoltage) {
+        setTargetAngle(targetAngle);
+        setTargetVoltage(targetVoltage);
+    }
+
+    private Rotation2d getAngle() {
+        return Rotation2d.fromRotations(angleEncoder.getSignal(CANcoderSignal.POSITION));
+    }
+
+    private void setTargetAngle(Rotation2d targetAngle) {
+        armMasterMotor.setControl(positionRequest.withPosition(targetAngle.getRotations()));
+    }
+
+    private void setTargetVoltage(double targetVoltage) {
+        ArmConstants.END_EFFECTOR_MECHANISM.setTargetVelocity(targetVoltage);
+        endEffectorMotor.setControl(voltageRequest.withOutput(targetVoltage));
+    }
+
+    private Pose3d calculateVisualizationPose() {
+        final Transform3d armTransform = new Transform3d(
+                new Translation3d(),
+                new Rotation3d(0, getAngle().getRadians(), 0)
+        );
+        return ArmConstants.ARM_VISUALIZATION_ORIGIN_POINT.transformBy(armTransform);
+    }
+}
