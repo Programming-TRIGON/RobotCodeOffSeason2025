f5bed14625f3a4ee6ad7e534564a673014245cd4
Strflightmight09 <148347057+Strflightmight09@users.noreply.github.com>
2025-09-11 15:15:44 +0300

    Added LEDConstants

--- src/main/java/frc/trigon/robot/constants/LEDConstants.java
+++ src/main/java/frc/trigon/robot/constants/LEDConstants.java
@@ -1,20 +1,33 @@
 package frc.trigon.robot.constants;
 
 import edu.wpi.first.wpilibj.util.Color;
+import lib.hardware.misc.leds.AddressableLEDStrip;
+import lib.hardware.misc.leds.LEDStrip;
 import lib.hardware.misc.leds.LEDStripAnimationSettings;
 
 public class LEDConstants {
-    //TODO: Implement LEDConstants
     public static LEDStripAnimationSettings.ColorFlowSettings COLLECTION_CONFIRMATION_ANIMATION_SETTINGS = new LEDStripAnimationSettings.ColorFlowSettings(
             Color.kGreen,
             0.5,
             false
     );
-    public static final LEDStripAnimationSettings.BlinkSettings CLIMB_ANIMATION_SETTINGS = new LEDStripAnimationSettings.BlinkSettings(Color.kYellow, 0.1);//TODO: Add fade animation
+    public static final LEDStripAnimationSettings.SingleFadeSettings CLIMB_ANIMATION_SETTINGS = new LEDStripAnimationSettings.SingleFadeSettings(Color.kYellow, 0.5);
+
+    private static final boolean INVERTED = false;
+    private static final int
+            NUMBER_OF_LEDS = 60,
+            INDEX_OFFSET = 0,
+            LED_STRIP_PORT = 0;
+    private static final LEDStrip LED_STRIP = LEDStrip.createAddressableLEDStrip(
+            INVERTED,
+            NUMBER_OF_LEDS,
+            INDEX_OFFSET
+    );
 
     /**
      * Initializes LEDConstants. Needed to be called for the LED strips to be initialized before being used.
      */
     public static void init() {
+        AddressableLEDStrip.initiateAddressableLED(LED_STRIP_PORT, NUMBER_OF_LEDS);
     }
 }
