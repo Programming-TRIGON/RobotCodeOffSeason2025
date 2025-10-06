0f718f0598ab1494f06eba7e10a70a9f200c3b0f
Shm <shmaya.rosenblatt@gmail.com>
2025-10-06 07:05:05 -0400

    ji
    Co-Authored-By: Nummun14 <142012009+Nummun14@users.noreply.github.com>

--- src/main/java/frc/trigon/robot/subsystems/swerve/SwerveConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/swerve/SwerveConstants.java
@@ -61,7 +61,7 @@ public class SwerveConstants {
     private static final PIDConstants
             TRANSLATION_PID_CONSTANTS = RobotHardwareStats.isSimulation() ?
             new PIDConstants(5, 0, 0) :
-            new PIDConstants(5, 0, 0),
+            new PIDConstants(4.5, 0, 0),
             PROFILED_ROTATION_PID_CONSTANTS = RobotHardwareStats.isSimulation() ?
                     new PIDConstants(4, 0, 0) :
                     new PIDConstants(13, 0, 0.25);
